0f25b45f662d192f121a0b6b291f264682d48833
Vince <babyaivan86@gmail.com>
2026-03-26 19:26:13 +0800

    feat: added file for solver.

---
+++ Solvers.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef SOLVERS_H
+#define SOLVERS_H
+
+#include "Matrix.hpp"
+#include "Vector.hpp"
+
+template <typename T>
+class Solvers {
+public:
+	static Vector<T> gaussianElimination(Matrix<T> A, Vector<T> b);
+	static Vector<T> gaussJordanElimination(Matrix<T> A, Vector<T> b);
+	static Vector<T> luDecomposition(Matrix<T> A, Vector<T> b);
+	static Vector<T> choleskyDecomposition(Matrix<T> A, Vector<T> b);
+	static Vector<T> qrDecomposition(Matrix<T> A, Vector<T> b);
+};
+
+#endif // !SOLVERS_H
+
