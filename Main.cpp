60f950dd6a321933812b349b78599d635333d8d8
Vince <babyaivan86@gmail.com>
2026-03-28 22:16:52 +0800

    feat: preparing for Linear Regression implementation.

--- Main.cpp
+++ Main.cpp
@@ -6,17 +6,6 @@
 using namespace std;
 
 int main() {
-	//testing the solver using gaussian elimination method.
-	Matrix<float> A{
-		{1.0f, 2.0f, 1.0f},
-		{3.0f, 8.0f, 1.0f},
-		{0.0f, 4.0f, 1.0f}
-	};
-
-	Vector<float> b{ 2.0f, 12.0f, 2.0f };
-
-	Vector<float> x = Solvers<float>::gaussianElimination(A, b);
-	cout << "Solution using Gaussian Elimination: " << x << endl;
 
 	return 0;
 }
